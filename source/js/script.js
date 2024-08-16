5654092f9adf8cde27c2dcb7b7220686366029f3
Ruslan Ayubov <zeiddho@gmail.com>
2024-08-16 19:50:12 +0300

    доработка макета в мастере

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,13 @@
+const buttonOpened = document.querySelector(".header__burger");
+const menu = document.querySelector('.header__nav');
+const buttonClosed = document.querySelector('.header__close')
 
+buttonOpened.addEventListener('click', () => {
+  menu.classList.add('header__nav--showed');
+  buttonClosed.addEventListener('click', closeMenu);
+});
+
+const closeMenu = () => {
+  menu.classList.remove('header__nav--showed');
+  buttonClosed.removeEventListener('click', closeMenu);
+};
