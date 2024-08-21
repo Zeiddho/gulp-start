70a04ea3235f1218b59c269ff7d669fd8f05ade1
Ruslan Ayubov <zeiddho@gmail.com>
2024-08-21 13:59:27 +0300

    1 задание по js  исправил

--- source/js/script.js
+++ source/js/script.js
@@ -1,13 +1,19 @@
 const buttonOpened = document.querySelector(".header__burger");
 const menu = document.querySelector('.header__nav');
-const buttonClosed = document.querySelector('.header__close')
-
-buttonOpened.addEventListener('click', () => {
-  menu.classList.add('header__nav--showed');
-  buttonClosed.addEventListener('click', closeMenu);
-});
+const buttonClosed = document.querySelector('.header__close');
 
 const closeMenu = () => {
   menu.classList.remove('header__nav--showed');
   buttonClosed.removeEventListener('click', closeMenu);
 };
+
+if (buttonOpened && menu) {
+  buttonOpened.addEventListener('click', () => {
+    menu.classList.add('header__nav--showed');
+
+    if (buttonClosed) {
+      buttonClosed.addEventListener('click', closeMenu);
+    }
+  });
+}
+
